598379a2604057fd07dd02c7430a39d1feae133f
Fernando Azuaje <fjah17@gmail.com>
2019-05-28 22:43:41 -0400

    agregando estilos y manejando clases parael modal

--- curso/src/js/home.js
+++ curso/src/js/home.js
@@ -36,9 +36,26 @@
         return data.data.movies
     }
 
+    const $home = document.getElementById('home')
+    const $featuringContainer = document.getElementById('featuring')
+
+    function setAttributes($element, attributes) {
+        for (const attribute in attributes) {
+          $element.setAttribute(attribute, attributes[attribute]);
+        }
+      }
+
     const $form = document.getElementById('form')
     $form.addEventListener('submit', (event) => {
         event.preventDefault()
+        $home.classList.add('search-active')
+        const $loader = document.createElement('img');
+        setAttributes($loader, {
+            src: 'src/images/loader.gif',
+            height: 50,
+            width: 50,
+        })
+        $featuringContainer.append($loader);
     })
 
     const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
@@ -66,7 +83,8 @@
 
     function addEventClick($element){
         $element.addEventListener('click', () => {
-            alert('click')
+            //alert('click')
+            showModal()
         })
     }
 
@@ -95,9 +113,8 @@
     
     //-------------------SELECTORES---------------------------------------///
     //-------------------SELECTORES---------------------------------------///
-    const $featuringContainer = document.getElementById('featuring')
+       
     
-    const $home = document.getElementById('home')
     
 
    // const $home = $('.home') //jquery method for id or class
@@ -108,6 +125,16 @@
     const $modalTitle = $modal.querySelector('h1')
     const $modalImage = $modal.querySelector('img')
     const $modalDescription = $modal.querySelector('p')
+
+    function showModal(){
+        $overlay.classList.add('active')
+        $modal.style.animation = 'modalIn .8s forwards'
+    }
+
+    $hideModal.addEventListener('click', () => {
+        $overlay.classList.remove('active')
+        $modal.style.animation = 'modalOut .8s forwards'
+    })
 
 })()
 
